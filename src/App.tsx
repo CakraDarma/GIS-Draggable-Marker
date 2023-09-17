ded15fb3903c4961059aa4239b1894b61cb5c5e2
CakraDarma <baguscakraa02@gmail.com>
2023-09-17 12:58:44 +0800

    add static location

--- src/App.tsx
+++ src/App.tsx
@@ -4,74 +4,78 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import RecenterMap from './components/RecenterMap';
 
 function App() {
-  // api web
-  const [geolocation, setGeolocation] = useState<LatLngExpression | null>(null);
-  const markerRef = useRef<any>(null);
+	// api web
+	const [geolocation, setGeolocation] = useState<LatLngExpression | null>(null);
+	const markerRef = useRef<any>(null);
 
-  const eventHandler = useMemo(
-    () => ({
-      dragend() {
-        const marker = markerRef.current;
+	const eventHandler = useMemo(
+		() => ({
+			dragend() {
+				const marker = markerRef.current;
 
-        if (marker) {
-          const position = marker.getLatLng();
-          setGeolocation(position);
-        }
-      },
-    }),
-    [],
-  );
+				if (marker) {
+					const position = marker.getLatLng();
+					setGeolocation(position);
+				}
+			},
+		}),
+		[]
+	);
 
-  useEffect(() => {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        setGeolocation({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      });
-    } else {
-      console.log('Your device is not available geolocation');
-    }
-  }, []);
+	useEffect(() => {
+		setGeolocation({
+			lat: -8.639152628370512,
+			lng: 115.19347429275514,
+		});
+	}, []);
 
-  if (!geolocation) {
-    return (
-      <div>
-        <h1>Upp Something Wrong...</h1>
-      </div>
-    );
-  }
+	// useEffect(() => {
+	// 	if ('geolocation' in navigator) {
+	// 		navigator.geolocation.getCurrentPosition(function (position) {
+	// 			setGeolocation({
+	// 				lat: position.coords.latitude,
+	// 				lng: position.coords.longitude,
+	// 			});
+	// 		});
+	// 	} else {
+	// 		console.log('Your device is not available geolocation');
+	// 	}
+	// }, []);
 
-  return (
-    <section
-      style={{
-        height: '100vh',
-        width: '100vw',
-      }}
-    >
-      <MapContainer
-        center={geolocation}
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{
-          height: '100%',
-        }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker ref={markerRef} position={geolocation} draggable={true} eventHandlers={eventHandler}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+	if (!geolocation) {
+		return (
+			<div>
+				<h1>Upp Something Wrong...</h1>
+			</div>
+		);
+	}
 
-        <RecenterMap location={geolocation} />
-      </MapContainer>
-    </section>
-  );
+	return (
+		<MapContainer
+			center={geolocation}
+			zoom={13}
+			scrollWheelZoom={true}
+			style={{
+				height: '100vh',
+				width: '100vw',
+			}}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+			/>
+			<Marker
+				ref={markerRef}
+				position={geolocation}
+				draggable={true}
+				eventHandlers={eventHandler}
+			>
+				<Popup>Cakra Home.</Popup>
+			</Marker>
+
+			<RecenterMap location={geolocation} />
+		</MapContainer>
+	);
 }
 
 export default App;
